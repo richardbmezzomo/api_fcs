942f91e6667ea0dfccbc2c62e98f4e612fddb746
richardbmezzomo <richardmezzomo1@gmail.com>
2025-06-25 20:26:44 -0300

    refactor: adiciona pastas para dentro da pasta src

---
+++ src/controllers/userController.js
@@ -0,0 +1,49 @@
+import db from "../models/index.js";
+import jwt from "jsonwebtoken";
+import { sendConfirmationEmail } from "../utils/emailService.js";
+const { User } = db;
+
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ where: { email } });
+
+    if (userExists) {
+      return res.status(400).json({ message: "Usuário já cadastrado!" });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      isEmailConfirmed: false,
+    });
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    const confirmationUrl = `${process.env.BASE_URL}/confirm-email?token=${token}`;
+
+    try {
+      await sendConfirmationEmail(email, confirmationUrl);
+    } catch (emailError) {
+      console.error("Erro ao enviar e-mail:", emailError);
+      return res.status(500).json({
+        message:
+          "Usuário cadastrado, mas não foi possível enviar o e-mail de confirmação.",
+      });
+    }
+
+    return res.status(201).json({
+      message: "Usuário cadastrado com sucesso!",
+      user: { id: user.id, name: user.name, email: user.email },
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Erro ao cadastrar usuário", error });
+  }
+};
